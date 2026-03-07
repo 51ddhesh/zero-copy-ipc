d81791a46892f136876f2d608ca1e9c471e13052
siddhesh <173941233+51ddhesh@users.noreply.github.com>
2026-03-07 15:44:15 +0000

    add BSL license notice

--- tests/test_shared_region.cpp
+++ tests/test_shared_region.cpp
@@ -1,3 +1,8 @@
+// Copyright 2026 51ddhesh
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #include "ipc/shared_region.hpp"
 
 #include <gtest/gtest.h>
